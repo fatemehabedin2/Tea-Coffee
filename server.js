2dd59c34e188cd2b24e12636a5ce17a3d14f2a3c
Thi Thanh Nguyen Nguyen <nguyenntt89@gmail.com>
2022-02-24 09:21:58 -0500

    Install, setup and configure development environment

---
+++ server.js
@@ -0,0 +1,73 @@
+//#region COMMENT HEADER
+/* Author: Group 1
+   Date: Feb 23,2022
+   Title: Project Phase 2
+
+*/
+//#endregion
+
+//#region Server Setup
+var express = require("express");
+var app = express();
+var HTTP_PORT = process.env.PORT || 8080;
+
+require("dotenv").config();
+
+function OnHttpStart() {
+  console.log("Express server started on port: " + HTTP_PORT);
+}
+
+app.use(express.static("views"));
+app.use(express.static("public"));
+
+const { engine } = require("express-handlebars");
+
+app.engine(".hbs", engine({ extname: ".hbs" }));
+
+app.set("view engine", ".hbs");
+
+//#endregion
+
+//#region Routes
+/*
+ */
+//#endregion
+
+//#region General Pages
+app.get("/", (req, res) => {
+  res.render("home", { layout: false });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { layout: false });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { layout: false });
+});
+
+//#endregion
+
+//#region Authentication
+app.get("/login", (req, res) => {
+    res.render("login", { layout: false });
+  });
+  app.get("/logout", (req, res) => {
+      //todo logout stuff
+    res.redirect("/")
+  });
+//#endregion
+
+//#region AuthorizedUsers
+/*
+ */
+//#endregion AdminPages
+
+//#region AdminPages
+/*
+ */
+//#endregion
+
+//#region Custom Functions and Startup
+app.listen(HTTP_PORT, OnHttpStart);
+//#endregion
